d3c2da6045c38d9fd69c89ad516b2f8d0f083bff
abishek2002tk <abishek2002tk@gmail.com>
2025-12-19 23:00:58 +0530

    cors issue solved

--- server/app.js
+++ server/app.js
@@ -16,14 +16,12 @@ app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
-// app.use(
-//     cors({
-//         origin: process.env.CLIENT_URL || "http://localhost:5173",
-//         credentials: true,
-//     })
-// );
-
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "http://localhost:5173",
+        credentials: true,
+    })
+);
 
 app.get("/", (req, res) => {
     res.send("server is running....");
